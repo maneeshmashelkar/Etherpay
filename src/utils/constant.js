f87c97ccca3727aeb4b3ffa28817b2ea9ec8a8e1
Maneesh <manishmashelkar45@gmail.com>
2023-01-20 23:02:26 +0530

    first commit

---
+++ src/utils/constant.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractABI = abi.abi;
+export const contractAddress = "0x1a8A5F46c99e7731aC5C9AAA309Fed8b8165798e";
---
+++ src/utils/formatDate.js
@@ -0,0 +1,16 @@
+export const formatDate = (date) => {
+  const str =
+    ("00" + date.getDate()).slice(-2) +
+    "/" +
+    ("00" + (date.getMonth() + 1)).slice(-2) +
+    "/" +
+    date.getFullYear() +
+    " " +
+    ("00" + date.getHours()).slice(-2) +
+    ":" +
+    ("00" + date.getMinutes()).slice(-2) +
+    ":" +
+    ("00" + date.getSeconds()).slice(-2);
+
+  return str;
+};
